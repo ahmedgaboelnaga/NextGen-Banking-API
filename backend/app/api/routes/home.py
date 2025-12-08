da7752f7772b682013b60ce89b7b0f3495ea9a03
Ahmed aboelnaga <ahmedgaboelnaga@gmail.com>
2025-12-08 23:25:45 +0200

    feat: add logging with loguru

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
+from app.core.logging import get_logger
+
+logger = get_logger()
 
 router = APIRouter(prefix="/home", tags=["home"])
 
 
 @router.get("/")
 async def home():
+    logger.info("Home endpoint accessed")
+    logger.debug("Debugging home endpoint")
+    logger.error("Error level log for testing")
+    logger.warning("Home endpoint warning log")
+    logger.critical("Home endpoint critical log")
     return {"message": "Welcome to the Next-Gen Backend API!"}
---
+++ backend/app/core/logging.py
@@ -0,0 +1,42 @@
+import os
+from loguru import logger
+from app.core.config import settings
+
+logger.remove()
+
+LOG_DIR= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'logs')
+
+LOG_FORMAT = "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {name}:{function}:{line} | {message}"
+
+LOG_FORMAT = (
+    "{time:YYYY-MM-DD HH:mm:ss.SSS} | "
+    "{level: <8} | "
+    "{name}:{function}:{line} | "
+    "{message}"
+)
+
+logger.add(
+    sink=os.path.join(LOG_DIR, "debug.log"),
+    format=LOG_FORMAT,
+    level="DEBUG" if settings.ENVIRONMENT == "development" else "INFO",
+    filter=lambda record: record["level"].no <= logger.level("WARNING").no,
+    rotation="10 MB",
+    retention="30 days",
+    compression="zip",
+    enqueue=True
+)
+
+logger.add(
+    sink=os.path.join(LOG_DIR, "error.log"),
+    format=LOG_FORMAT,
+    level="ERROR",
+    rotation="10 MB",
+    retention="30 days",
+    compression="zip",
+    backtrace=True,
+    diagnose=True,
+    enqueue=True
+)
+
+def get_logger():
+    return logger
