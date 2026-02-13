e96204fb8b19ace376aa28f041cf5c478ae400b4
Ahmed aboelnaga <ahmedgaboelnaga@gmail.com>
2026-02-13 22:02:36 +0200

    feat: add user schemas and user model

---
+++ backend/app/auth/models.py
@@ -0,0 +1,47 @@
+import uuid
+from datetime import datetime
+from sqlmodel import Field, Column
+from pydantic import computed_field
+from sqlalchemy.dialects import postgresql as pg
+from sqlalchemy import func, text
+from backend.app.auth.schema import BaseUserSchema, RoleChoicesSchema
+
+
+class User(BaseUserSchema, table=True):
+    id: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID(as_uuid=True),
+            primary_key=True,
+        ),
+        default_factory=uuid.uuid4,
+    )
+    hashed_password: str
+    failed_login_attempts: int = Field(default=0, sa_type=pg.SMALLINT)
+    last_failed_login: datetime | None = Field(sa_column=Column(pg.TIMESTAMP(timezone=True)))
+    otp: str = Field(max_length=6, default="")
+    otp_expiary_time: datetime | None = Field(sa_column=Column(pg.TIMESTAMP(timezone=True)))
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(),
+        sa_column=Column(
+            pg.TIMESTAMP(timezone=True),
+            nullabale=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+        ),
+    )
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(),
+        sa_column=Column(
+            pg.TIMESTAMP(timezone=True),
+            nullabale=False,
+            onupdate=func.current_timestamp(),
+        ),
+    )
+
+    @computed_field
+    @property
+    def full_name(self) -> str:
+        full_name = f"{self.first_name} {self.middle_name + ' ' if self.middle_name else ''}{self.last_name}"
+        return full_name.title().strip()
+    
+    def has_role(self, role: RoleChoicesSchema) -> bool:
+        return self.role.value == role.value
---
+++ backend/app/auth/schema.py
@@ -0,0 +1,72 @@
+from enum import Enum
+from sqlmodel import SQLModel, Field
+from pydantic import EmailStr, field_validator
+from fastapi import HTTPException, status
+
+
+class SecurityQuestionSchema(str, Enum):
+    MOTHER_MAIDEN_NAME = "mother_median_name"
+    CHILDHOOD_FRIEND = "childhood friend"
+    FAVORITE_COLOR = "favorite_color"
+    BIRTH_CITY = "birth_city"
+    FIRST_SCHOOL = "first_school"
+
+    @classmethod
+    def get_description(cls, value: "SecurityQuestionSchema") -> str:
+        descriptions = {
+            cls.MOTHER_MAIDEN_NAME: "What is your mother's maiden name?",
+            cls.CHILDHOOD_FRIEND: "Who was your childhood friend?",
+            cls.FAVORITE_COLOR: "What is your favorite color?",
+            cls.BIRTH_CITY: "In which city were you born?",
+            cls.FIRST_SCHOOL: "What was the name of your first school?",
+        }
+        return descriptions.get(value, "Unknown security question")
+
+
+class AccountStatusSchema(str, Enum):
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+    LOCKED = "locked"
+    PENDING = "pending"
+
+
+class RoleChoicesSchema(str, Enum):
+    CUSTOMER = "customer"
+    ACCOUNT_EXECUTIVE = "account_executive"
+    BRANCH_MANAGER = "branch_manager"
+    ADMIN = "admin"
+    SUPER_ADMIN = "super_admin"
+    TELLER = "teller"
+
+
+class BaseUserSchema(SQLModel):
+    username: str | None = Field(default=None, max_length=12, unique=True)
+    email: EmailStr = Field(default=None, unique=True, index=True, max_length=255)
+    first_name: str = Field(max_length=30)
+    middle_name: str | None = Field(default=None, max_length=30)
+    last_name: str = Field(max_length=30)
+    id_no: int = Field(unique=True, gt=0)
+    is_active: bool = False
+    is_superuser: bool = False
+    security_question: SecurityQuestionSchema = Field(max_length=30)
+    security_answer: str = Field(max_length=30)
+    account_status: AccountStatusSchema = Field(default=AccountStatusSchema.INACTIVE)
+    role: RoleChoicesSchema = Field(default=RoleChoicesSchema.CUSTOMER)
+
+
+class UserCreateSchema(BaseUserSchema):
+    password: str = Field(min_length=8, max_length=40)
+    confirm_password: str = Field(min_length=8, max_length=40)
+
+    @field_validator("confirm_password")
+    def passwords_match(cls, v, values):
+        if "password" in values and v != values["password"]:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "status": "error",
+                    "message": "Passwords do not match",
+                    "action": "Please ensure that the password and confirm password fields match.",
+                },
+            )
+        return v
